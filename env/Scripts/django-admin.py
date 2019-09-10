056b5a048f3940a12efa93623002d2b80285065f
Brian Ruiz <brianruiz123@yahoo.com>
2019-09-09 23:30:25 -0500

    Update

---
+++ env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\Brian Ruiz\Documents\VS Code\Web\Django-webapp-blog\env\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
