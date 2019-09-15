cf310193fd469a58c898d0e488f3ad23e187895c
Brian Ruiz <brianruiz123@yahoo.com>
2019-09-15 15:59:32 -0500

    database

--- django_project/blog/models.py
+++ django_project/blog/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User  # 
 
 # Create your models here.
+
+# model for user post's
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    date_posted = models.DateTimeField(default=timezone.now)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)  # Authorizes user to post, on_delete erases post if user is deleted
+
+    def __str__(self):
+        return self.title
+    
+    
