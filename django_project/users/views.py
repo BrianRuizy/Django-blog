d1960ab9f8130f36af6fcccaeb6f2624fdbcd34e
Brian Ruiz <brianruiz123@yahoo.com>
2019-09-22 01:51:54 -0500

    update user registration

--- django_project/users/views.py
+++ django_project/users/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
 
-# Create your views here.
+
 def register(request):
-    # going to be using django created forms
-    form = UserCreationForm()
+    if request.method =='POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            messages.succes(request, f'')
+    else:
+        form = UserCreationForm()
     return render(request, 'users/register.html', {'form': form})
