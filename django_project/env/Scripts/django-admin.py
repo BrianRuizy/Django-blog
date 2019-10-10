006dc3c2e5de70c908c198a49242028abda24685
Brian Ruiz <brianruiz123@yahoo.com>
2019-10-09 21:52:46 -0500

    added .gitignore, fixed viruatlenv

--- django_project/env/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!C:\Users\bruiz\repos\Django-blog\django_project\env\Scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
